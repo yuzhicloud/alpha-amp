f5d278640d93a473e9ff4e00b2355d1e8e4606c7
billyhotjava <billy.xiezhimin@gmail.com>
2018-10-17 11:30:54 +0800

    init

---
+++ src/main/java/com/yzcloud/alpha/amp/AlphaAmpApplication.java
@@ -0,0 +1,12 @@
+package com.yzcloud.alpha.amp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaAmpApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaAmpApplication.class, args);
+	}
+}
